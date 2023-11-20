7c1c81d3723990d3a4faa57352fcb376dbce8824
Nika V <gordievkyn@gmail.com>
2023-11-20 14:18:11 +0300

    Add project files.

---
+++ USRS3/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[] massiv = new int[10]
+{
+	1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+};
+
+Console.WriteLine("количество чётных " + massiv.Count(x => x % 2 == 0));
+Console.WriteLine("количество нечётных " + massiv.Count(x => x % 2 == 1));
